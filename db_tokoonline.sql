c664d74257132e6d6fa9675ba354f856c01010e2
arifrusman <arif83.ar@gmail.com>
2020-09-17 10:53:50 +0700

    meng update database

--- db_tokoonline.sql
+++ db_tokoonline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2020 at 08:11 AM
+-- Generation Time: Sep 17, 2020 at 05:52 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.1.24
 
@@ -41,6 +41,25 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_lengkap`) VALUES
 (1, 'arif', '1234', 'Arif Rusman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(5) NOT NULL,
+  `nama_kategori` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Kategori A'),
+(2, 'Kategori B');
 
 -- --------------------------------------------------------
 
@@ -84,7 +103,35 @@ CREATE TABLE `pelanggan` (
 INSERT INTO `pelanggan` (`id_pelanggan`, `email_pelanggan`, `password_pelanggan`, `nama_pelanggan`, `telepon_pelanggan`, `alamat_pelanggan`) VALUES
 (1, 'doddy@gmail.com', 'doddy', 'Doddy Setiawan', '08888123123', ''),
 (2, 'erik@gmail.com', 'erik', 'Erik Candra', '0888111111', ''),
-(3, 'yudi@contoh.com', '1234', 'Yudi Saputra', '0746356643', 'Jogja');
+(3, 'yudi@contoh.com', '1234', 'Yudi Saputra', '0746356643', 'Jogja'),
+(4, 'doni@contoh.com', '1234', 'Doni', '0761888888', 'Semarang'),
+(5, 'erwin@contoh.com', '1234', 'Erwin', '088867553', 'Demak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_pembelian` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `bank` varchar(255) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `bukti` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_pembelian`, `nama`, `bank`, `jumlah`, `tanggal`, `bukti`) VALUES
+(4, 11, 'Erik', 'Mandiri', 2020000, '2020-08-05', '20200805165815tugas3.png'),
+(5, 13, 'Erik', 'Mandiri', 6020000, '2020-08-08', '20200808173932punya-putri.png'),
+(6, 18, 'Doni', 'Mandiri', 6044000, '2020-09-15', '20200915032134database.png'),
+(7, 19, 'Erwin', 'Mandiri', 3024000, '2020-09-15', '20200915045855database.png');
 
 -- --------------------------------------------------------
 
@@ -95,29 +142,41 @@ INSERT INTO `pelanggan` (`id_pelanggan`, `email_pelanggan`, `password_pelanggan`
 CREATE TABLE `pembelian` (
   `id_pembelian` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
-  `id_ongkir` int(11) NOT NULL,
   `tanggal_pembelian` date NOT NULL,
   `total_pembelian` int(11) NOT NULL,
-  `nama_kota` varchar(100) NOT NULL,
-  `tarif` int(11) NOT NULL,
   `alamat_pengiriman` text NOT NULL,
-  `status_pembelian` varchar(100) NOT NULL DEFAULT 'pending'
+  `status_pembelian` varchar(100) NOT NULL DEFAULT 'pending',
+  `resi_pengiriman` varchar(50) NOT NULL,
+  `totalberat` int(11) NOT NULL,
+  `provinsi` varchar(255) NOT NULL,
+  `distrik` varchar(255) NOT NULL,
+  `tipe` varchar(255) NOT NULL,
+  `kodepos` varchar(255) NOT NULL,
+  `ekspedisi` varchar(255) NOT NULL,
+  `paket` varchar(255) NOT NULL,
+  `ongkir` int(11) NOT NULL,
+  `estimasi` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pembelian`
 --
 
-INSERT INTO `pembelian` (`id_pembelian`, `id_pelanggan`, `id_ongkir`, `tanggal_pembelian`, `total_pembelian`, `nama_kota`, `tarif`, `alamat_pengiriman`, `status_pembelian`) VALUES
-(1, 1, 1, '2020-07-25', 1500000, '', 0, '', 'pending'),
-(2, 1, 1, '2020-07-26', 500000, '', 0, '', 'pending'),
-(6, 1, 1, '2020-07-29', 2520000, '', 0, '', 'pending'),
-(7, 1, 1, '2020-07-29', 2520000, '', 0, '', 'pending'),
-(8, 1, 2, '2020-07-29', 2530000, '', 0, '', 'pending'),
-(9, 2, 1, '2020-08-01', 1520000, '', 0, '', 'pending'),
-(10, 2, 1, '2020-08-01', 1520000, '', 0, '', 'pending'),
-(11, 2, 1, '2020-08-01', 2020000, 'Demak', 20000, '', 'pending'),
-(12, 2, 1, '2020-08-01', 2020000, 'Demak', 20000, 'jl. Serayu kode pos: 5611122', 'pending');
+INSERT INTO `pembelian` (`id_pembelian`, `id_pelanggan`, `tanggal_pembelian`, `total_pembelian`, `alamat_pengiriman`, `status_pembelian`, `resi_pengiriman`, `totalberat`, `provinsi`, `distrik`, `tipe`, `kodepos`, `ekspedisi`, `paket`, `ongkir`, `estimasi`) VALUES
+(1, 1, '2020-07-25', 1500000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(2, 1, '2020-07-26', 500000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(6, 1, '2020-07-29', 2520000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(7, 1, '2020-07-29', 2520000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(8, 1, '2020-07-29', 2530000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(9, 2, '2020-08-01', 1520000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(10, 2, '2020-08-01', 1520000, '', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(11, 2, '2020-08-01', 2020000, '', 'barang dikirim', 'abc123', 0, '', '', '', '', '', '', 0, ''),
+(12, 2, '2020-08-01', 2020000, 'jl. Serayu kode pos: 5611122', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(13, 2, '2020-08-08', 6020000, 'Waru ', 'sudah kirim pembayaran', '', 0, '', '', '', '', '', '', 0, ''),
+(16, 3, '2020-08-15', 8030000, 'Jl. Sultan, Cirebon', 'pending', '', 0, '', '', '', '', '', '', 0, ''),
+(17, 3, '2020-09-13', 6024000, 'Jl. Hastina 2 Wedomartani Sleman', 'pending', '', 4000, 'DI Yogyakarta', 'Sleman', 'Kabupaten', '55513', 'jne', 'CTC', 24000, '1-2'),
+(18, 4, '2020-09-15', 6044000, 'jl. Pemuda 287 Semarang', 'barang dikirim', 'smg000343', 4000, 'Jawa Tengah', 'Semarang', 'Kota', '50135', 'jne', 'REG', 44000, '2-3'),
+(19, 5, '2020-09-15', 3024000, 'jl. Sedayu no 14 Demak', 'barang dikirim', 'Dmk0034345', 2000, 'Jawa Tengah', 'Demak', 'Kabupaten', '59519', 'jne', 'OKE', 24000, '6-7');
 
 -- --------------------------------------------------------
 
@@ -157,7 +216,14 @@ INSERT INTO `pembelian_produk` (`id_pembelian_produk`, `id_pembelian`, `id_produ
 (13, 0, 1, 1, 'Laptop Asus xix', 1500000, 1000, 1000, 1500000),
 (14, 0, 2, 1, 'Laptop Acer 212', 500000, 1000, 1000, 500000),
 (15, 12, 1, 1, 'Laptop Asus xix', 1500000, 1000, 1000, 1500000),
-(16, 12, 2, 1, 'Laptop Acer 212', 500000, 1000, 1000, 500000);
+(16, 12, 2, 1, 'Laptop Acer 212', 500000, 1000, 1000, 500000),
+(17, 13, 2, 2, 'Laptop Acer 212', 500000, 1000, 2000, 1000000),
+(18, 13, 3, 2, 'Laptop macbook 212', 2500000, 1200, 2400, 5000000),
+(23, 16, 3, 2, 'Laptop macbook 212', 2500000, 1200, 2400, 5000000),
+(24, 16, 4, 1, 'Laptop Pro', 3000000, 2000, 2000, 3000000),
+(25, 17, 4, 2, 'Laptop Pro', 3000000, 2000, 4000, 6000000),
+(26, 18, 4, 2, 'Laptop Pro', 3000000, 2000, 4000, 6000000),
+(27, 19, 1, 2, 'Laptop Asus xix', 1500000, 1000, 2000, 3000000);
 
 -- --------------------------------------------------------
 
@@ -167,22 +233,48 @@ INSERT INTO `pembelian_produk` (`id_pembelian_produk`, `id_pembelian`, `id_produ
 
 CREATE TABLE `produk` (
   `id_produk` int(11) NOT NULL,
+  `id_kategori` int(5) NOT NULL,
   `nama_produk` varchar(100) NOT NULL,
   `harga_produk` int(11) NOT NULL,
   `berat_produk` int(11) NOT NULL,
   `foto_produk` varchar(100) NOT NULL,
-  `deskripsi_produk` text NOT NULL
+  `deskripsi_produk` text NOT NULL,
+  `stok_produk` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga_produk`, `berat_produk`, `foto_produk`, `deskripsi_produk`) VALUES
-(1, 'Laptop Asus xix', 1500000, 1000, 'kari-shea-1SAnrIxw5OY-unsplash.jpg', 'Ini asus bagus'),
-(2, 'Laptop Acer 212', 500000, 1000, 'sora-sagano-WFSap6CIXuw-unsplash (1).jpg', 'Ini Acer bagus'),
-(3, 'Laptop macbook 212', 2500000, 1200, 'ben-kolde-t9DooibgMEk-unsplash.jpg', 'Ini macbook bagus bergaransi'),
-(4, 'Laptop Pro', 3000000, 2000, 'kitai-jogia-zhvaeh-R9rA-unsplash.jpg', 'Laptop pro');
+INSERT INTO `produk` (`id_produk`, `id_kategori`, `nama_produk`, `harga_produk`, `berat_produk`, `foto_produk`, `deskripsi_produk`, `stok_produk`) VALUES
+(1, 1, 'Laptop Asus xix', 1500000, 1000, 'kari-shea-1SAnrIxw5OY-unsplash.jpg', 'Ini asus bagus', 3),
+(2, 1, 'Laptop Acer 212', 500000, 1000, 'sora-sagano-WFSap6CIXuw-unsplash (1).jpg', 'Ini Acer bagus', 3),
+(3, 2, 'Laptop macbook 212', 2500000, 1200, 'ben-kolde-t9DooibgMEk-unsplash.jpg', 'Ini macbook bagus bergaransi', 3),
+(4, 2, 'Laptop Pro', 3000000, 2000, 'kitai-jogia-zhvaeh-R9rA-unsplash.jpg', 'Laptop pro', 3),
+(5, 1, 'ASD edited', 500000, 50, 'p5290175.jpg', '                    asdasd    asd            ', 10),
+(6, 2, 'Buku Koding', 60000, 600, 'sincerely-media-CXYPfveiuis-unsplash.jpg', 'Buku koding terbaru', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk_foto`
+--
+
+CREATE TABLE `produk_foto` (
+  `id_produk_foto` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `nama_produk_foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produk_foto`
+--
+
+INSERT INTO `produk_foto` (`id_produk_foto`, `id_produk`, `nama_produk_foto`) VALUES
+(2, 8, 'vinicius-amnx-amano-fdiXdOdYtLE-unsplash.jpg'),
+(3, 8, 'sincerely-media-CXYPfveiuis-unsplash.jpg'),
+(5, 8, '20200905085618sincerely-media-CXYPfveiuis-unsplash.jpg'),
+(6, 6, 'sincerely-media-CXYPfveiuis-unsplash.jpg');
 
 --
 -- Indexes for dumped tables
@@ -195,6 +287,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
 -- Indexes for table `ongkir`
 --
 ALTER TABLE `ongkir`
@@ -205,6 +303,12 @@ ALTER TABLE `ongkir`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
 
 --
 -- Indexes for table `pembelian`
@@ -225,6 +329,12 @@ ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`);
 
 --
+-- Indexes for table `produk_foto`
+--
+ALTER TABLE `produk_foto`
+  ADD PRIMARY KEY (`id_produk_foto`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -233,6 +343,12 @@ ALTER TABLE `produk`
 --
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `ongkir`
@@ -244,25 +360,37 @@ ALTER TABLE `ongkir`
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `pembelian_produk`
 --
 ALTER TABLE `pembelian_produk`
-  MODIFY `id_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `produk_foto`
+--
+ALTER TABLE `produk_foto`
+  MODIFY `id_produk_foto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
